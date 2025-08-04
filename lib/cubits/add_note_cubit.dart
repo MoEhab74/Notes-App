9a6997e964931c7e1968cfce46ccc636c3fa8d16
MoEhab74 <mohamedehabswe74@gmail.com>
2025-08-04 13:54:13 +0300

    Register Adaptor

--- lib/cubits/add_note_cubit.dart
+++ lib/cubits/add_note_cubit.dart
@@ -1,11 +1,13 @@
 import 'package:notes_app/cubits/notes_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/models/note_model.dart';
 
 class AddNoteCubit extends Cubit<AddNotesState> {
   AddNoteCubit() : super(AddNoteInitial());
 
-  void addNote({required String title, required String subTitle}){
-
+  void addNote(NoteModel note)
+  {
+    // Store the NoteModel in ur local data base
   }
 
 }
